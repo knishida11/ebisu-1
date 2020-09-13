8f930d44a814ff6122030e40080da7ac72007a25
Keigo Nishida <keigo.nishida.1480@gmail.com>
2020-09-14 05:49:11 +0900

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+
+  // object-fit-images
+  objectFitImages();
+
+  // Swiper
+  new Swiper(".swiper-container", {
+    loop: true,
+    speed: 500,
+    effect: "fade",
+    loopAdditionalSlides: 1,
+    autoplay: {
+      delay: 5000,
+    },
+    breakpoints: {
+      768: {
+        height: 400
+      }
+    }
+  });
+
+  // wow.js
+  new WOW().init();
+
+  // smooth scrolling
+  $('a[href^="#"]').click(function () {
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - $("#js-header").outerHeight();
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+  // burger menu for mobile
+  $(".header__burgerIconOuter").on("click", function () {
+    if ($('.header__burgerIcon').hasClass("header__burger--open")) {
+      $('.header__burgerIcon').removeClass("header__burger--open");
+      $(".header__navLinks").removeClass("open");
+      $("body").removeClass("header--lock");
+    } else {
+      $('.header__burgerIcon').addClass("header__burger--open");
+      $(".header__navLinks").addClass("open");
+      $("body").addClass("header--lock");
+    }
+  });
+});
